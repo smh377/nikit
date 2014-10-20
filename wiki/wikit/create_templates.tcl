2a0466d3de176d7b82662b35e4f63ddaa3fa1f59
Jos Decoster <jos.decoster@gmail.com>
2014-10-20 22:47:41 +0200

    bootstrap style for new page

--- wiki/wikit/create_templates.tcl
+++ wiki/wikit/create_templates.tcl
@@ -172,7 +172,7 @@ ${Content}
     <meta name="author" content="">
     <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
-    <title>Signin Template for Bootstrap</title>
+    <title>Login</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
@@ -221,7 +221,7 @@ ${Content}
     <meta name="author" content="">
     <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
-    <title>Signin Template for Bootstrap</title>
+    <title>Session login</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
@@ -354,29 +354,51 @@ ${Content}
 
 ################################################################################
 
-    set new {<!DOCTYPE HTML>
-<html lang='en'>
-<head>
-<title>New page</title>
-<<include:TEMPLATE:cssjs>>
-</head>
-<body>
-  <div class='edit'>
-    <div class='header'>
-      <div class='logo'><a href='http://wiki.tcl.tk' class='logo'>wiki.tcl.tk</a><img class='logo' alt='' src='/plume.png'></div>
-      <div class='title'>Create new page</div>
-    </div>
-  </div>
-  <p>Enter a new page name:</p>
-  <form method='post' action='/new' id='new'>
-    <fieldset title='New' id='login'>
-      <input title='Name' name='pagename' type='text' value='' tabindex='1' size='80' autofocus>
-      <input name='save' type='submit' value='Create'>
-    </fieldset>
-  </form>
-  <script type='text/javascript' src='/scripts/wiki.js'></script>
-</body>
-</html>}
+    set new {<!DOCTYPE html>
+<!-- saved from url=(0040)http://getbootstrap.com/examples/signin/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>New page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/signin.css" rel="stylesheet">
+
+    <script src="/scripts/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <link rel="stylesheet" type="text/css" href="chrome-extension://cgndfbhngibokieehnjhbjkkhbfmhojo/css/validation.css"></head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" role="form" method='post' action="/new" id="new">
+        <h2 class="form-signin-heading">New page</h2>
+        <p>Enter a new page name.</p>
+        <input type="text" class="form-control" placeholder="Name" required="" autofocus="" name="pagename">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Create</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/scripts/ie10-viewport-bug-workaround.js"></script>
+
+</body></html>}
 
 ################################################################################
 
@@ -536,7 +558,7 @@ ${Content}
     <meta name="author" content="">
     <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
-    <title>Signin Template for Bootstrap</title>
+    <title>Upload</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
